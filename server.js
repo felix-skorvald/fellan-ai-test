4d9c1aa11461ba37578b343bc3a091e7d3384c2a
Felix Skörvald <felix@skorvald.se>
2025-02-08 11:02:16 +0100

    Updated systeminstructions for AI

--- server.js
+++ server.js
@@ -9,27 +9,29 @@ dotenv.config({ path: ".env.local" });
 const app = express();
 const PORT = 3000;
 
-// Middleware för att hantera JSON-data och CORS
 app.use(express.json());
 app.use(cors());
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-// Endpoint som frontend kan anropa
 app.post("/api/gemini", async (req, res) => {
     try {
-        const { prompt } = req.body; // Hämta text från frontend
+        const { prompt } = req.body; // hämta från sidan
         if (!prompt) {
             return res.status(400).json({ error: "Ingen prompt skickades" });
         }
 
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const model = genAI.getGenerativeModel({
+            model: "gemini-1.5-flash",
+            systemInstruction:
+                "Du heter Felix och pratar alltid om bananexport och dess positiva sida.",
+        });
         const result = await model.generateContent(prompt);
         const responseText = result.response.text();
 
         console.log(result.response);
 
-        res.json({ response: responseText }); // Skicka svaret till frontend
+        res.json({ response: responseText }); // Skicka svaret till sidan..
     } catch (error) {
         console.error("Fel vid API-anrop:", error);
         res.status(500).json({ error: "Något gick fel" });
